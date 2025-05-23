a7498d0ee3399262b5c33cd251bdcf56b7c1dacc
LKaway96 <dreamataiwan@gmail.com>
2025-05-23 11:20:24 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ let facemesh;
 let video;
 let predictions = [];
 const points = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
+const leftEye1 = [243, 190, 56, 28, 27, 29, 30, 247, 130, 25, 110, 24, 23, 22, 26, 112];
+const leftEye2 = [133, 173, 157, 158, 159, 160, 161, 246, 33, 7, 163, 144, 145, 153, 154, 155];
 
 function setup() {
   createCanvas(640, 480).position((windowWidth - 640) / 2, (windowHeight - 480) / 2);
@@ -34,9 +36,27 @@ function draw() {
 
   if (predictions.length > 0) {
     const keypoints = predictions[0].scaledMesh;
+
+    // Draw main points
     beginShape();
     for (let i = 0; i < points.length; i++) {
       const [x, y] = keypoints[points[i]];
+      vertex(x, y);
+    }
+    endShape(CLOSE);
+
+    // Draw left eye (first set)
+    beginShape();
+    for (let i = 0; i < leftEye1.length; i++) {
+      const [x, y] = keypoints[leftEye1[i]];
+      vertex(x, y);
+    }
+    endShape(CLOSE);
+
+    // Draw left eye (second set)
+    beginShape();
+    for (let i = 0; i < leftEye2.length; i++) {
+      const [x, y] = keypoints[leftEye2[i]];
       vertex(x, y);
     }
     endShape(CLOSE);
